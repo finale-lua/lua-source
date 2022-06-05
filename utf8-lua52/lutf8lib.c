b71311d7ee2bb7432976b4e4f3d3ac76d0cd5051
Robert Patterson <robert@robertgpatterson.com>
2022-06-05 14:03:55 -0500

    win build

--- utf8-lua52/lutf8lib.c
+++ utf8-lua52/lutf8lib.c
@@ -128,30 +128,29 @@ static int codepoint (lua_State *L) {
 static size_t code_to_utf8(char *const buffer, const lua_Integer code)
 {
     if (code <= 0x7F) {
-        buffer[0] = code;
+        buffer[0] = (char)code;
         return 1;
     }
     if (code <= 0x7FF) {
-        buffer[0] = 0xC0 | (code >> 6);            /* 110xxxxx */
-        buffer[1] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        buffer[0] = (char)(0xC0 | (code >> 6));            /* 110xxxxx */
+        buffer[1] = (char)(0x80 | (code & 0x3F));          /* 10xxxxxx */
         return 2;
     }
     if (code <= 0xFFFF) {
-        buffer[0] = 0xE0 | (code >> 12);           /* 1110xxxx */
-        buffer[1] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
-        buffer[2] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        buffer[0] = (char)(0xE0 | (code >> 12));           /* 1110xxxx */
+        buffer[1] = (char)(0x80 | ((code >> 6) & 0x3F));   /* 10xxxxxx */
+        buffer[2] = (char)(0x80 | (code & 0x3F));          /* 10xxxxxx */
         return 3;
     }
     if (code <= 0x10FFFF) {
-        buffer[0] = 0xF0 | (code >> 18);           /* 11110xxx */
-        buffer[1] = 0x80 | ((code >> 12) & 0x3F);  /* 10xxxxxx */
-        buffer[2] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
-        buffer[3] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        buffer[0] = (char)(0xF0 | (code >> 18));           /* 11110xxx */
+        buffer[1] = (char)(0x80 | ((code >> 12) & 0x3F));  /* 10xxxxxx */
+        buffer[2] = (char)(0x80 | ((code >> 6) & 0x3F));   /* 10xxxxxx */
+        buffer[3] = (char)(0x80 | (code & 0x3F));          /* 10xxxxxx */
         return 4;
     }
     return 0;
 }
-
 
 static void pushutfchar (lua_State *L, int arg) {
   lua_Integer code = luaL_checkinteger(L, arg);
