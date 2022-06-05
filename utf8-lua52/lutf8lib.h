ff9dd0f9f738d9dcdf6eea08f9eb59d52c81278b
Robert Patterson <robert@robertgpatterson.com>
2022-06-04 20:38:13 -0500

    backport lutf8lib.c for Lua 5.2 and allow for stand-alone inclusion

---
+++ utf8-lua52/lutf8lib.h
@@ -0,0 +1,15 @@
+//
+//  lutf8lib.h
+//  Lua
+//
+//  Created by Robert Patterson on 6/3/22.
+//
+
+#ifndef lutf8lib_h
+#define lutf8lib_h
+
+#include "lua.h"
+
+LUAMOD_API int luaopen_utf8 (lua_State *L);
+
+#endif /* lutf8lib_h */
